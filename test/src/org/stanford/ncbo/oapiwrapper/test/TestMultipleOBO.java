3109bc6c7c6eb2c5263c6e1bcd89590e7d0708e4
msalvadores <msalvadores@gmail.com>
2012-12-12 16:48:52 -0800

    More tests.

---
+++ test/src/org/stanford/ncbo/oapiwrapper/test/TestMultipleOBO.java
@@ -0,0 +1,7 @@
+package org.stanford.ncbo.oapiwrapper.test;
+
+public class TestMultipleOBO {
+	private static final String inputRepositoryFolder = "./test/repo/input/evoc";
+	private static final String outputRepositoryFolder = "./test/repo/output/evoc";
+	private static final String masterFileName = "evoc_2.9_obo/obo/";
+}
