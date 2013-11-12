3ba9a08ef1657cb7836afa8bd8bd1d4ea15f49d6
msalvadores <msalvadores@gmail.com>
2013-11-12 11:39:06 -0800

    treeView property added for obo

--- src/org/stanford/ncbo/oapiwrapper/OntologyParser.java
+++ src/org/stanford/ncbo/oapiwrapper/OntologyParser.java
@@ -193,26 +193,44 @@ public class OntologyParser {
 						if (!some.getProperty().isAnonymous() && !some.getFiller().isAnonymous()) {
 							String propSome = some.getProperty().asOWLObjectProperty().getIRI().toString().toLowerCase();
 							if (propSome.contains("obo")) {
-								if (propSome.endsWith("part_of") || propSome.endsWith("bfo_0000050") ||
+								if (propSome.endsWith("subClassOf")) {
+									OWLAnnotationProperty prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/treeView"));
+									OWLAxiom annAsse = fact.getOWLAnnotationAssertionAxiom(prop, 
+											sc.getSubClass().asOWLClass().getIRI(),
+											some.getFiller().asOWLClass().getIRI());
+									allAxioms.add(annAsse);
+								} else if (propSome.endsWith("part_of") || propSome.endsWith("bfo_0000050") ||
 										propSome.endsWith("contains") || propSome.endsWith("ro_0001019") || 
 										propSome.endsWith("develops_from") || propSome.endsWith("ro_0002202") ) {
 									OWLAnnotationProperty prop = null;
 									if (propSome.endsWith("contains") || propSome.endsWith("ro_0001019")) {
-										OWLSubClassOfAxiom ax = fact.getOWLSubClassOfAxiom(some.getFiller(),sc.getSubClass());
-										allAxioms.add(ax);
+										//OWLSubClassOfAxiom ax = fact.getOWLSubClassOfAxiom(some.getFiller(),sc.getSubClass());
+										//allAxioms.add(ax);
 										prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/obo/contains"));
 										OWLAxiom annAsse = fact.getOWLAnnotationAssertionAxiom(prop, 
 												some.getFiller().asOWLClass().getIRI(),
 												sc.getSubClass().asOWLClass().getIRI());
 										allAxioms.add(annAsse);
+
+										prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/treeView"));
+										annAsse = fact.getOWLAnnotationAssertionAxiom(prop, 
+												some.getFiller().asOWLClass().getIRI(),
+												sc.getSubClass().asOWLClass().getIRI());
+										allAxioms.add(annAsse);
 									} else {
-										OWLSubClassOfAxiom ax = fact.getOWLSubClassOfAxiom(sc.getSubClass(), some.getFiller());
-										allAxioms.add(ax);
+										//OWLSubClassOfAxiom ax = fact.getOWLSubClassOfAxiom(sc.getSubClass(), some.getFiller());
+										//allAxioms.add(ax);
 										if (propSome.endsWith("part_of") || propSome.endsWith("bfo_0000050"))
 											prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/obo/part_of"));
 										else
 											prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/obo/develops_from"));
 										OWLAxiom annAsse = fact.getOWLAnnotationAssertionAxiom(prop, 
+												sc.getSubClass().asOWLClass().getIRI(),
+												some.getFiller().asOWLClass().getIRI());
+										allAxioms.add(annAsse);
+										
+										prop = fact.getOWLAnnotationProperty(IRI.create("http://data.bioontology.org/metadata/treeView"));
+										annAsse = fact.getOWLAnnotationAssertionAxiom(prop, 
 												sc.getSubClass().asOWLClass().getIRI(),
 												some.getFiller().asOWLClass().getIRI());
 										allAxioms.add(annAsse);
