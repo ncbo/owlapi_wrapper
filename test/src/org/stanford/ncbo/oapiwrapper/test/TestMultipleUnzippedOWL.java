2ef0f2a120f1892768d1e40c21f3eac97f0224cb
Jennifer Vendetti <vendetti@stanford.edu>
2016-03-07 11:17:16 -0800

    Convert to JUnit 4

--- test/src/org/stanford/ncbo/oapiwrapper/test/TestMultipleUnzippedOWL.java
+++ test/src/org/stanford/ncbo/oapiwrapper/test/TestMultipleUnzippedOWL.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import junit.framework.TestCase;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.stanford.ncbo.oapiwrapper.OntologyBean;
@@ -13,17 +14,18 @@ import org.stanford.ncbo.oapiwrapper.OntologyParser;
 import org.stanford.ncbo.oapiwrapper.OntologyParserException;
 import org.stanford.ncbo.oapiwrapper.ParserInvocation;
 
-public class TestMultipleUnzippedOWL extends TestCase {
+import static org.junit.Assert.assertEquals;
+
+public class TestMultipleUnzippedOWL {
 	private static final String inputRepositoryFolder = "./test/repo/input/hsdb";
 	private static final String outputRepositoryFolder = "./test/repo/output/hsdb";
 	private static final String masterFileName = "HSDB_OCRe.owl";
 	
 	private final static Logger log = Logger.getLogger(TestMultipleUnzippedOWL.class.getName()); 
 
-	@Override
-    protected void setUp() throws Exception  {
-        super.setUp();
-    }
+	@Before
+	public void setUp() throws Exception  {
+	}
 	
 	@Test
 	public void testInputOK() {
