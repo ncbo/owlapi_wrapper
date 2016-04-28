23f97cdfc86b5170e4547a546d9c29d3d6685740
Jennifer Vendetti <vendetti@stanford.edu>
2016-04-28 15:18:37 -0700

    Fix / simplify metrics unit tests

--- src/test/java/org/stanford/ncbo/oapiwrapper/OntologyMetricsTest.java
+++ src/test/java/org/stanford/ncbo/oapiwrapper/OntologyMetricsTest.java
@@ -4,9 +4,10 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -14,32 +15,38 @@ import static org.junit.Assert.*;
 
 public class OntologyMetricsTest {
 
-    private String metricsPath;
+    private static final Logger logger = LoggerFactory.getLogger(OntologyMetricsTest.class);
 
-    @Before
-    public void setUp() throws Exception {
-        String outputFolder = "./src/test/resources/repo/output/pizza";
-        metricsPath = outputFolder + File.separator + OntologyParserConstants.METRICS_FILE;
-
-        File file = new File(metricsPath);
-        if (file.exists()) {
-            assertTrue(file.delete());
-        }
-
-        ParserInvocation pi = new ParserInvocation("./src/test/resources/repo/input/pizza", outputFolder, "pizza.owl", true);
+    @BeforeClass
+    public static void oneTimeSetup() throws Exception {
+        // Parse pizza ontology
+        logger.info("Parsing pizza ontology");
+        ParserInvocation pi = new ParserInvocation("./src/test/resources/repo/input/pizza",
+                "./src/test/resources/repo/output/pizza", "pizza.owl", true);
         OntologyParser parser = new OntologyParser(pi);
         assertTrue(parser.parse());
+        logger.info("Completed parsing pizza ontology\n");
+
+        // Parse HSDB ontology
+        logger.info("Parsing HSDB ontology");
+        pi = new ParserInvocation("./src/test/resources/repo/input/hsdb",
+                "./src/test/resources/repo/output/hsdb", "HSDB_OCRe.owl", true);
+        parser = new OntologyParser(pi);
+        assertTrue(parser.parse());
+        logger.info("Completed parsing HSDB ontology\n");
     }
 
     @Test
     public void generate_MetricsForPizza_FileCreated() throws Exception {
-        File file = new File(metricsPath);
+        String path = "./src/test/resources/repo/output/pizza" + File.separator + OntologyParserConstants.METRICS_FILE;
+        File file = new File(path);
         assertTrue(file.exists());
     }
 
     @Test
     public void generate_MetricsForPizza_2RecordsCreated() throws Exception {
-        String data = FileUtils.readFileToString(new File(metricsPath));
+        String path = "./src/test/resources/repo/output/pizza" + File.separator + OntologyParserConstants.METRICS_FILE;
+        String data = FileUtils.readFileToString(new File(path));
         CSVFormat format = CSVFormat.DEFAULT.withHeader(OntologyParserConstants.METRICS_FILE_HEADERS);
         CSVParser parser = CSVParser.parse(data, format);
 
@@ -48,24 +55,21 @@ public class OntologyMetricsTest {
     }
 
     @Test
-    public void generate_MetricsForPizza_ClassCountsCalculated() throws Exception {
-        CSVRecord record = getMetricsRecord();
-        String classCount = record.get(OntologyParserConstants.METRICS_CLASS_COUNT);
-        assertEquals(100, Integer.parseInt(classCount));
-    }
+    public void generate_MetricsForOntologyWithoutImports_Calculated() throws Exception {
+        String path = "./src/test/resources/repo/output/pizza" + File.separator + OntologyParserConstants.METRICS_FILE;
+        String data = FileUtils.readFileToString(new File(path));
+        CSVFormat format = CSVFormat.DEFAULT.withHeader(OntologyParserConstants.METRICS_FILE_HEADERS);
+        CSVParser parser = CSVParser.parse(data, format);
 
-    @Test
-    public void generate_MetricsForPizza_PropertyCountsCalculated() throws Exception {
-        CSVRecord record = getMetricsRecord();
-        String propertyCount = record.get(OntologyParserConstants.METRICS_PROPERY_COUNT);
-        assertEquals(8, Integer.parseInt(propertyCount));
-    }
+        // Start at 1 to skip header row
+        CSVRecord record = parser.getRecords().get(1);
+        int numClasses = Integer.parseInt(record.get(OntologyParserConstants.METRICS_CLASS_COUNT));
+        int numIndividuals = Integer.parseInt(record.get(OntologyParserConstants.METRICS_INDIVIDUAL_COUNT));
+        int numProperties = Integer.parseInt(record.get(OntologyParserConstants.METRICS_PROPERY_COUNT));
 
-    @Test
-    public void generate_MetricsForPizza_IndividualCountsCalculated() throws Exception {
-        CSVRecord record = getMetricsRecord();
-        String individualCount = record.get(OntologyParserConstants.METRICS_INDIVIDUAL_COUNT);
-        assertEquals(5, Integer.parseInt(individualCount));
+        int[] expected = { 100, 5, 8 };
+        int[] actual = { numClasses, numIndividuals, numProperties };
+        assertArrayEquals(expected, actual);
     }
 
     @Test
@@ -84,20 +88,6 @@ public class OntologyMetricsTest {
         int[] expected = { 356, 35, 208 };
         int[] actual = { numClasses, numIndividuals, numProperties };
         assertArrayEquals(expected, actual);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    private CSVRecord getMetricsRecord() throws Exception {
-        String data = FileUtils.readFileToString(new File(metricsPath));
-        CSVFormat format = CSVFormat.DEFAULT.withHeader(OntologyParserConstants.METRICS_FILE_HEADERS);
-        CSVParser parser = CSVParser.parse(data, format);
-
-        // Start at 1 to skip header row
-        return parser.getRecords().get(1);
     }
 
 }
