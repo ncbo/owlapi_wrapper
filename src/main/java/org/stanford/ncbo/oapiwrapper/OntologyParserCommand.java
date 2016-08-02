627acb93b16b98cf8da9fe53ec076034d3f55afd
Jennifer Vendetti <vendetti@stanford.edu>
2016-08-02 15:51:09 -0700

    Use Logback instead of java.util.logging

--- src/main/java/org/stanford/ncbo/oapiwrapper/OntologyParserCommand.java
+++ src/main/java/org/stanford/ncbo/oapiwrapper/OntologyParserCommand.java
@@ -1,17 +1,14 @@
 package org.stanford.ncbo.oapiwrapper;
 
-import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import java.io.File;
 
 public class OntologyParserCommand {
-	private final static Logger log = Logger.getLogger(OntologyParserCommand.class .getName()); 
+	private final static Logger log = LoggerFactory.getLogger(OntologyParserCommand.class .getName());
+
 	public static void main(String[] args) {
 		final Options gnuOptions = new Options(); 
 		gnuOptions.addOption("i", "input-repository", true, 
@@ -32,39 +29,39 @@ public class OntologyParserCommand {
 			String reasoner = call.getOptionValue("r");
 			Boolean bReasoner = reasoner == null || reasoner.equals("true");
 
-			ParserInvocation pi = new ParserInvocation(inputRepoPath,
-					outputRepoPath, masterFileName, bReasoner);
+			ParserInvocation pi = new ParserInvocation(inputRepoPath, outputRepoPath, masterFileName, bReasoner);
 			if (!pi.valid()) {
-				log.info("Parsing invocation with values " +pi.toString());
-				log.log(Level.SEVERE, "invocation is not valid");
-				log.log(Level.SEVERE,pi.getParserLog().toString());
+				log.info("Parsing invocation with values: {}", pi.toString());
+				log.error("Invalid invocation!");
+				log.error(pi.getParserLog().toString());
 				System.exit(-1);
 			}
-			log.info("Parsing invocation with values " +pi.toString());
+			log.info("Parsing invocation with values: {}", pi.toString());
 			
 			OntologyParser parser = null;
 			try {
 				parser = new OntologyParser(pi);
 			} catch (OntologyParserException e) {
-				log.log(Level.SEVERE, "Error creating parser", e);
-				log.log(Level.SEVERE,pi.getParserLog().toString());
+				log.error("Parser creation error", e);
+				log.error(pi.getParserLog().toString());
 				System.exit(-1);
 			}
 			boolean parseResult = false;
 			try {
 				parseResult = parser.parse();
 			} catch (Exception e) {
-				log.log(Level.SEVERE, "Error parsing", e);
-				log.log(Level.SEVERE,pi.getParserLog().toString());
+				log.error("Parsing error", e);
+				log.error(pi.getParserLog().toString());
 				System.exit(-1);
 			}
 			File f = new File(pi.getOutputRepositoryFolder() + File.separator + "owlapi.xrdf");
-			log.info("Parse result " + parseResult+ ".Output triples in " + f.getAbsolutePath());		
+			log.info("Parse result: {}", parseResult);
+			log.info("Output triples in: {}" + f.getAbsolutePath());
 		} catch (ParseException e) {
-			log.log(Level.SEVERE, e.getMessage());
+			log.error(e.getMessage());
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		log.info("Finished parsing.");
+		log.info("Finished parsing!");
 	}
 }
